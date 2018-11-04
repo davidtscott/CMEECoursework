552c29e8f7d120f163191dd3cbc4722a5246b54e
David Scott <david.scott18@imperial.ac.uk>
2018-11-04 00:04:30 +0000

    updated tree height shell script

--- Week3/Code/run_get_TreeHeight.sh
+++ Week3/Code/run_get_TreeHeight.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # Author: David Scott David.Scott18@imperial.ac.uk
 # Script: boilerplate.sh
-# Desc: simple boilerplate for shell scripts
+# Desc: runs rscript and python3 scripts (both cal. tree height)
 # Arguments: none
 # Date: Oct 2018
 
 Rscript --vanilla get_TreeHeight.R ../Data/trees.csv
+
+python3 get_TreeHeight.py ../Data/trees.csv
+
